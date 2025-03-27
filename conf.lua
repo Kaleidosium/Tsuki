c321799b806b46249b11c7bb3b3295632fac53a8
Dania Rifki <atomicnumberphi@gmail.com>
2025-03-27 09:29:23 +0700

    Remove yue dep in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,9 +1,8 @@
 -- https://love2d.org/wiki/Config_Files
 
 function love.conf(t)
-    require("yue")
-    t.console = true
     t.window.title = "My Tsuki Game"
     t.window.width = 800
     t.window.height = 480
+    t.console = true
 end
